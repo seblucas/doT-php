499ea215f2fa0fc173023910edbca52d44c1a6b0
nzn1 <netanel@nneuman.net>
2023-04-19 22:45:09 +0300

    Update doT.php
    Fix UTF-8 characters breaking templates.

--- doT.php
+++ doT.php
@@ -15,13 +15,13 @@ class doT {
 
     public function resolveDefs ($block) {
         $me = $this;
-        return preg_replace_callback ("/\{\{#([\s\S]+?)\}\}/", function ($m) use ($me) {
+        return preg_replace_callback ("/\{\{#([\s\S]+?)\}\}/u", function ($m) use ($me) {
             $d = $m[1];
             $d = substr ($d, 4);
             if (!array_key_exists ($d, $me->def)) {
                 return "";
             }
-            if (preg_match ("/\{\{#([\s\S]+?)\}\}/", $me->def [$d])) {
+            if (preg_match ("/\{\{#([\s\S]+?)\}\}/u", $me->def [$d])) {
                 return $me->resolveDefs ($me->def [$d], $me->def);
             } else {
                 return $me->def [$d];
@@ -30,12 +30,12 @@ class doT {
     }
 
     public function handleDotNotation ($string) {
-        $out = preg_replace ("/(\w+)\.(.*?)([\s,\)])/", "\$$1[\"$2\"]$3", $string);
-        $out = preg_replace ("/(\w+)\.([\w\.]*?)$/", "\$$1[\"$2\"] ", $out);
-        $out = preg_replace ("/\./", '"]["', $out);
+        $out = preg_replace ("/(\w+)\.(.*?)([\s,\)])/u", "\$$1[\"$2\"]$3", $string);
+        $out = preg_replace ("/(\w+)\.([\w\.]*?)$/u", "\$$1[\"$2\"] ", $out);
+        $out = preg_replace ("/\./u", '"]["', $out);
 
         // Special hideous case : shouldn't be committed
-        $out = preg_replace ("/^i /", ' $i ', $out);
+        $out = preg_replace ("/^i /u", ' $i ', $out);
         return $out;
     }
 
@@ -46,20 +46,20 @@ class doT {
 
         // deps
         if (empty ($def)) {
-            $func = preg_replace ("/\{\{#([\s\S]+?)\}\}/", "", $func);
+            $func = preg_replace ("/\{\{#([\s\S]+?)\}\}/u", "", $func);
         } else {
             $this->def = $def;
             $func = $this->resolveDefs ($func);
         }
 
-        $func = preg_replace ("/'|\\\/", "\\$&", $func);
+        $func = preg_replace ("/'|\\\/u", "\\$&", $func);
 
         // interpolate
-        $func = preg_replace_callback ("/\{\{=([\s\S]+?)\}\}/", function ($m) use ($me) {
+        $func = preg_replace_callback ("/\{\{=([\s\S]+?)\}\}/u", function ($m) use ($me) {
             return "' . " . $me->handleDotNotation ($m[1]) . " . '";
         }, $func);
         // Conditional
-        $func = preg_replace_callback ("/\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/", function ($m) use ($me) {
+        $func = preg_replace_callback ("/\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/u", function ($m) use ($me) {
             $elsecase = $m[1];
             $code = $m[2];
             if ($elsecase) {
